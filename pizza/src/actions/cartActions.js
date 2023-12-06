5a42e0373b561732336f939db396740526ca9588
Ajaykanth123 <131512357+Ajaykanth123@users.noreply.github.com>
2023-12-06 12:25:47 +0530

    Update cartActions.js

--- pizza/src/actions/cartActions.js
+++ pizza/src/actions/cartActions.js
@@ -1,1 +1,22 @@
-abcd
+
+export const addToCart = (pizza, quantity, varient) => (dispatch, getState) => {
+    var cartItem = {
+        name: pizza.name,
+        _id: pizza._id,
+        image: pizza.image,
+        varient: varient,
+        quantity: quantity,
+        prices: pizza.Prices,
+        price: pizza.Prices[0][varient] * quantity
+    };
+
+    // Dispatch the action first
+    dispatch({ type: 'ADD_TO_CART', payload: cartItem });
+
+    // Access the state after dispatch
+    const cartItems = getState().cartReducer.cartItems
+
+    // Update local storage
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+};
+
