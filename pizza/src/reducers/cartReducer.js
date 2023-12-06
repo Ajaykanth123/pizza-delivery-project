c6df695810ad04105f75bbc90bb5b72df46d4fa1
Ajaykanth123 <131512357+Ajaykanth123@users.noreply.github.com>
2023-12-06 12:31:51 +0530

    Create cartReducer.js

---
+++ pizza/src/reducers/cartReducer.js
@@ -0,0 +1,17 @@
+export const cartReducer =(state={cartItems :[]} ,action)=>{
+
+    switch (action.type) {
+        case 'ADD_TO_CART':
+
+        return{
+            ...state ,
+            cartItems :[...state.cartItems ,action.payload]
+           // cartItems: state.cartItems.concat(action.payload)
+        }
+            
+         default: return state
+    
+    }
+    
+}
+  
